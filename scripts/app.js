dcfd32254b2a95d5884aa3a7035be36925d3bc06
missmercy0310 <mistrexxmercy@gmail.com>
2021-09-28 14:34:15 -0700

    started js game object

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,18 @@
 console.log("Welcome to Creature Keeper");
 
+const game = {
+    hunger: 0,
+    boredom: 0,
+    bloodlust: 0,
+    time: 60,
+    arrow: $("#arrow"),
+    select: $("#select"),
+    power: $("#power"),
+
+    powerUp (event) {
+        console.log("Powering On...")
+        $("#middle-half").append("<h2>Welcome!</h2>")
+    }
+};
+
+game.power.click(game.powerUp.bind(game));
